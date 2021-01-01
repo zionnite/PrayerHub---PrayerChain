9a6381c3a4b7784b1255addca3cea55893390133
Nosakhare Atekha Endurance <nosakharee.16@gmail.com>
2021-01-01 04:15:11 -0800

    PrayerHub, this is an initial commit for prayerHub Payer Chain

---
+++ lib/util.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+Color backgroundColorPrimary = Color(0xff673AB7);
+Color backgroundColorLight = Color(0xff7c43bd);
+Color backgroundColorDark = Color(0xff512DA8);
+Color backgroundColorAccent = Color(0xff7C4DFF);
+Color textColorWhite = Color(0xffffffff);
