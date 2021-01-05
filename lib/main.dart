d740b1b061ddfef10b9545b5cd454e076dab8708
Nosakhare Atekha Endurance <nosakharee.16@gmail.com>
2021-01-04 22:32:04 -0800

    PrayerHub, this is an initial commit for prayerHub Payer Chain

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:prayer_hub/pages/login_page.dart';
+import 'package:prayer_hub/util.dart';
 
 void main() {
   runApp(MyApp());
@@ -33,9 +37,107 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(widget.title),
-      ),
+      backgroundColor: backgroundColorPrimary,
+      body: SingleChildScrollView(
+          child: Center(
+        child: Stack(
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height,
+              width: double.maxFinite,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: ExactAssetImage("assets/images/prayer.jpeg"),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: ClipRRect(
+                // make sure we apply clip it properly
+                child: BackdropFilter(
+                  filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+                  child: Container(
+                    color: Colors.grey.withOpacity(0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        RaisedButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => LoginPage(),
+                              ),
+                            );
+                          },
+                          color: Colors.black12,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              'Login',
+                              style: TextStyle(
+                                color: textColorWhite,
+                                fontSize: 23.0,
+                                fontFamily: 'NerkoOne',
+                              ),
+                            ),
+                          ),
+                        ),
+                        RaisedButton(
+                          color: textColorWhite,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              'Register',
+                              style: TextStyle(
+                                color: textColorWhite,
+                                fontSize: 23.0,
+                                fontFamily: 'NerkoOne',
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      )),
     );
   }
 }
+
+//
+// Positioned(
+// bottom: 345.0,
+// left: 155.0,
+// child: Row(
+// crossAxisAlignment: CrossAxisAlignment.center,
+// mainAxisAlignment: MainAxisAlignment.center,
+// children: [
+// RaisedButton(
+// color: textColorWhite,
+// child: Text(
+// 'Login',
+// style: TextStyle(
+// color: textColorWhite,
+// fontSize: 23.0,
+// ),
+// ),
+// ),
+// RaisedButton(
+// color: textColorWhite,
+// child: Text(
+// 'Register',
+// style: TextStyle(
+// color: textColorWhite,
+// fontSize: 23.0,
+// ),
+// ),
+// ),
+// ],
+// ),
+// ),
---
+++ lib/model/user_profile.dart
@@ -0,0 +1,78 @@
+import 'dart:convert';
+
+UserProfile userProfileFromJson(String str) =>
+    UserProfile.fromJson(json.decode(str));
+
+String userProfileToJson(UserProfile data) => json.encode(data.toJson());
+
+class UserProfile {
+  UserProfile({
+    this.id,
+    this.fullName,
+    this.userName,
+    this.age,
+    this.sex,
+    this.phoneNo,
+    this.expertise,
+    this.userImg,
+    this.isBan,
+    this.isVerify,
+    this.isActivated,
+    this.city,
+    this.state,
+    this.country,
+    this.address,
+  });
+
+  String id;
+  String fullName;
+  String userName;
+  String age;
+  String sex;
+  String phoneNo;
+  String expertise;
+  String userImg;
+  String isBan;
+  String isVerify;
+  String isActivated;
+  String city;
+  String state;
+  String country;
+  String address;
+
+  factory UserProfile.fromJson(Map<String, dynamic> json) => UserProfile(
+        id: json["id"],
+        fullName: json["full_name"],
+        userName: json["user_name"],
+        age: json["age"],
+        sex: json["sex"],
+        phoneNo: json["phone_no"],
+        expertise: json["expertise"],
+        userImg: json["user_img"],
+        isBan: json["is_ban"],
+        isVerify: json["is_verify"],
+        isActivated: json["is_activated"],
+        city: json["city"],
+        state: json["state"],
+        country: json["country"],
+        address: json["address"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "full_name": fullName,
+        "user_name": userName,
+        "age": age,
+        "sex": sex,
+        "phone_no": phoneNo,
+        "expertise": expertise,
+        "user_img": userImg,
+        "is_ban": isBan,
+        "is_verify": isVerify,
+        "is_activated": isActivated,
+        "city": city,
+        "state": state,
+        "country": country,
+        "address": address,
+      };
+}
